9a7457a35d28b39f79a13cced561e356a99b6224
Rerurate514 <rerurateyuto@gmail.com>
2026-05-29 17:21:50 +0900

    partBuilderに変更

--- lib/builder.dart
+++ lib/builder.dart
@@ -3,8 +3,8 @@ import 'package:source_gen/source_gen.dart';
 import 'package:theme_palette_generator/src/theme_annotation_generator.dart';
 
 Builder themePaletteBuilder(BuilderOptions options) {
-  return LibraryBuilder(
-    ThemeAnnotationGenerator(),
-    generatedExtension: '.theme.g.dart',
+  return PartBuilder(
+    [ThemeAnnotationGenerator()],
+    '.theme.g.dart',
   );
 }
