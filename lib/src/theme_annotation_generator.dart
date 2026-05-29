b3fb734b4d298948376b1d82dc0faf19cd4a5a63
Rerurate514 <rerurateyuto@gmail.com>
2026-05-29 17:20:31 +0900

    freezed式に変更

--- lib/src/theme_annotation_generator.dart
+++ lib/src/theme_annotation_generator.dart
@@ -20,27 +20,69 @@ class ThemeAnnotationGenerator extends GeneratorForAnnotation<ThemePalette> {
       );
     }
 
-    final className = element.name!; 
+    final className = element.name!;
 
-    final fields = element.fields.where((field) {
-      final typeName = field.type.getDisplayString();
-      return typeName == 'Color';
-    });
+    if (element.constructors.isEmpty) {
+      throw InvalidGenerationSourceError(
+        'The class must have at least one constructor.',
+        element: element,
+      );
+    }
 
-    final themeMixin = Mixin(
+    final constructor = element.constructors.firstWhere(
+      (c) => c.isFactory,
+      orElse: () => throw InvalidGenerationSourceError(
+        'The class must have a default factory constructor.',
+        element: element,
+      ),
+    );
+
+    final colorParams = constructor.formalParameters.where(
+      (p) => p.type.getDisplayString() == 'Color',
+    ).toList();
+
+    final abstractClass = Class(
       (b) => b
         ..name = '_\$$className'
-        ..on = refer('ThemeExtension<$className>')
+        ..abstract = true
+        ..extend = refer('ThemeExtension<$className>')
+        ..constructors.add(Constructor((c) => c..constant = true))
         ..methods.addAll(
-          buildMethods(className, fields)
+          buildMethods(className, colorParams)
         ),
     );
 
+    final implClass = Class(
+      (b) => b
+        ..name = '_$className'
+        ..extend = refer(className)
+        ..constructors.add(Constructor((c) => c
+          ..constant = true
+          ..optionalParameters.addAll(colorParams.map((p) => Parameter((p2) => p2
+            ..name = p.name!
+            ..type = refer('Color')
+            ..named = true
+            ..required = true)))
+          ..initializers.addAll(colorParams.map((p) => refer('this').property(p.name!).assign(refer(p.name!)).code))))
+        ..fields.addAll(colorParams.map((p) => Field((f) => f
+          ..name = p.name
+          ..type = refer('Color')
+          ..modifier = FieldModifier.final$
+          ..annotations.add(refer('override'))))),
+    );
+
     final emitter = DartEmitter(useNullSafetySyntax: true);
-    final rawCode = themeMixin.accept(emitter).toString();
+    final abstractCode = abstractClass.accept(emitter).toString();
+    final implCode = implClass.accept(emitter).toString();
+
+    final finalCode = '''
+$abstractCode
+
+$implCode
+''';
 
     return DartFormatter(
       languageVersion: DartFormatter.latestLanguageVersion,
-    ).format(rawCode);
+    ).format(finalCode);
   }
 }
