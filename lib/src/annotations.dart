8a2dd12ac6508fa3b8460a632b01d704da7c5c0f
Rerurate514 <rerurateyuto@gmail.com>
2026-05-29 17:41:40 +0900

    アノテーションの値を追加

--- lib/src/annotations.dart
+++ lib/src/annotations.dart
@@ -1,3 +1,11 @@
+import 'package:flutter/material.dart';
+
 class ThemePalette {
-  const ThemePalette();
+  const ThemePalette({
+    required this.brightness,
+    this.fromSeed
+  });
+  
+  final Brightness brightness;
+  final Color? fromSeed;
 }
