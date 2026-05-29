3f20ed20ea82aa1d433e045833e15905b9f709c9
Rerurate514 <rerurateyuto@gmail.com>
2026-05-29 19:18:36 +0900

    build_methodsにメソッドを追加

--- lib/src/gens/build_methods.dart
+++ lib/src/gens/build_methods.dart
@@ -1,60 +1,166 @@
 import 'package:code_builder/code_builder.dart';
 import 'package:analyzer/dart/element/element.dart';
 
-Iterable<Method> buildMethods(String className, Iterable<Element> parameters) {
-  final list = <Method>[];
+Iterable<Method> buildMethods(
+  String className,
+  Iterable<FormalParameterElement> parameters,
+) {
+  final params = parameters.toList();
 
-  for (final param in parameters) {
-    list.add(Method((m) => m
-      ..name = param.name
-      ..type = MethodType.getter
-      ..returns = refer('Color')));
-  }
+  return [
+    // abstract getters
+    ...params.map(
+      (p) => Method(
+        (m) => m
+          ..name = p.name!
+          ..type = MethodType.getter
+          ..returns = refer('Color'),
+      ),
+    ),
 
-  list.add(Method((m) => m
-    ..name = 'copyWith'
-    ..annotations.add(refer('override'))
-    ..returns = refer(className)
-    ..optionalParameters.addAll(parameters.map((p) => Parameter((p2) => p2
-      ..name = p.name!
-      ..type = refer('Color?')
-      ..named = true)))
-    ..body = refer('_$className').call([], {
-      for (final p in parameters) p.name!: refer(p.name!).ifNullThen(refer('this').property(p.name!))
-    }).code));
+    // copyWith
+    Method(
+      (m) => m
+        ..name = 'copyWith'
+        ..annotations.add(refer('override'))
+        ..returns = refer(className)
+        ..optionalParameters.addAll(params.map(
+            (p) => Parameter(
+              (p2) => p2
+                ..name = p.name!
+                ..type = refer('Color?')
+                ..named = true,
+            ),
+          ),),
+    ),
 
-  list.add(Method((m) => m
-    ..name = 'lerp'
-    ..annotations.add(refer('override'))
-    ..returns = refer('ThemeExtension<$className>')
-    ..requiredParameters.addAll([
-      Parameter((p) => p..name = 'other'..type = refer('ThemeExtension<$className>?')),
-      Parameter((p) => p..name = 't'..type = refer('double')),
-    ])
-    ..body = Block.of([
-      Code('if (other is! $className) return this;'),
-      refer('_$className').call([], {
-        for (final p in parameters)
-          p.name!: refer('Color')
-              .property('lerp')
-              .call([refer(p.name!), refer('other').property(p.name!), refer('t')])
-              .nullChecked
-      }).returned.statement,
-    ])));
+    // lerp
+    Method(
+      (m) => m
+        ..name = 'lerp'
+        ..annotations.add(refer('override'))
+        ..returns = refer('ThemeExtension<$className>')
+        ..requiredParameters.addAll([
+          Parameter(
+            (p) => p
+              ..name = 'other'
+              ..type = refer('ThemeExtension<$className>?'),
+          ),
+          Parameter(
+            (p) => p
+              ..name = 't'
+              ..type = refer('double'),
+          ),
+        ])
+        ..body = Block.of([
+          Code('if (other is! $className) return this;'),
+          refer('_$className')
+              .call([], {
+                for (final p in parameters)
+                  p.name!: refer('Color').property('lerp').call([
+                    refer(p.name!),
+                    refer('other').property(p.name!),
+                    refer('t'),
+                  ]).nullChecked,
+              })
+              .returned
+              .statement,
+        ]),
+    ),
 
-  list.add(Method((m) => m
-    ..name = 'of'
-    ..static = true
-    ..returns = refer(className)
-    ..requiredParameters.add(Parameter((p) => p..name = 'context'..type = refer('BuildContext')))
-    ..body = refer('Theme')
-        .property('of')
-        .call([refer('context')])
-        .property('extension')
-        .call([])
-        .nullChecked
-        .returned
-        .statement));
+    // of(BuildContext)
+    Method(
+      (m) => m
+        ..name = 'of'
+        ..static = true
+        ..returns = refer(className)
+        ..requiredParameters.add(
+          Parameter(
+            (p) => p
+              ..name = 'context'
+              ..type = refer('BuildContext'),
+          ),
+        )
+        ..body = refer('Theme')
+            .property('of')
+            .call([refer('context')])
+            .property('extension')
+            .call([])
+            .nullChecked
+            .returned
+            .statement,
+    ),
 
-  return list;
+    // toMap
+    Method(
+      (m) => m
+        ..name = 'toMap'
+        ..returns = refer('Map<String, Color>')
+        ..body = literalMap({
+          for (final p in params) literalString(p.name!): refer(p.name!),
+        }).returned.statement,
+    ),
+
+    // == operator
+    // == operator
+    Method(
+      (m) => m
+        ..name = 'operator =='
+        ..annotations.add(refer('override'))
+        ..returns = refer('bool')
+        ..requiredParameters.add(
+          Parameter(
+            (p) => p
+              ..name = 'other'
+              ..type = refer('Object'),
+          ),
+        )
+        ..body = Block.of([
+          Code('if (identical(this, other)) return true;'),
+          Code('if (other is! $className) return false;'),
+          Code(
+            'return ${params.map((p) => 'other.${p.name} == ${p.name}').join(' && ')};',
+          ),
+        ]),
+    ),
+
+    // hashCode
+    Method(
+      (m) => m
+        ..name = 'hashCode'
+        ..type = MethodType.getter
+        ..annotations.add(refer('override'))
+        ..returns = refer('int')
+        ..body = refer('Object')
+            .property('hash')
+            .call(params.map((p) => refer(p.name!)))
+            .returned
+            .statement,
+    ),
+
+    // debugFillProperties
+    Method(
+      (m) => m
+        ..name = 'debugFillProperties'
+        ..annotations.add(refer('override'))
+        ..returns = refer('void')
+        ..requiredParameters.add(
+          Parameter(
+            (p) => p
+              ..name = 'properties'
+              ..type = refer('DiagnosticPropertiesBuilder'),
+          ),
+        )
+        ..body = Block.of([
+          Code('super.debugFillProperties(properties);'),
+          ...params.map(
+            (p) => refer('properties').property('add').call([
+              refer(
+                'ColorProperty',
+              ).call([literalString(p.name!), refer(p.name!)]),
+            ]).statement,
+          ),
+        ]),
+    ),
+  ];
 }
