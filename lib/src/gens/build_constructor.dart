aa548d2d51785a5a28aee33de1eff9a42a44cefa
Rerurate514 <rerurateyuto@gmail.com>
2026-05-29 14:40:48 +0900

    コンストラクタbuildのメソッドを追加

---
+++ lib/src/gens/build_constructor.dart
@@ -0,0 +1,18 @@
+import 'package:analyzer/dart/element/element.dart';
+import 'package:code_builder/code_builder.dart';
+
+Constructor buildConstructor(Iterable<FieldElement> fields) {
+  return Constructor(
+    (c) => c
+      ..constant = true
+      ..requiredParameters.addAll(
+        fields.map(
+          (field) => Parameter(
+            (p) => p
+              ..name = field.name!
+              ..toThis = true,
+          ),
+        ),
+      ),
+  );
+}
