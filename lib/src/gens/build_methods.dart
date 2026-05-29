869239df2da885ef9bf3c0c566b941008dbaaac0
Rerurate514 <rerurateyuto@gmail.com>
2026-05-29 17:20:21 +0900

    methodsビルドを最適化

--- lib/src/gens/build_methods.dart
+++ lib/src/gens/build_methods.dart
@@ -1,94 +1,60 @@
-import 'package:analyzer/dart/element/element.dart';
 import 'package:code_builder/code_builder.dart';
+import 'package:analyzer/dart/element/element.dart';
 
-Iterable<Method> buildMethods(String className, Iterable<FieldElement> fields) {
-  final colorGetters = fields.map((field) => Method((m) => m
-    ..name = field.name
-    ..type = MethodType.getter
-    ..returns = refer('Color'),
-  ));
+Iterable<Method> buildMethods(String className, Iterable<Element> parameters) {
+  final list = <Method>[];
 
-  return [
-    ...colorGetters,
-    Method(
-      (m) => m
-        ..name = 'copyWith'
-        ..annotations.add(refer('override'))
-        ..returns = refer(className)
-        ..optionalParameters.addAll(
-          fields.map(
-            (field) => Parameter(
-              (p) => p
-                ..name = field.name!
-                ..type = refer('Color?')
-                ..named = true,
-            ),
-          ),
-        )
-        ..body = Block((b) {
-          final namedArgs = <String, Expression>{};
-          for (final field in fields) {
-            namedArgs[field.name!] = CodeExpression(
-              Code('${field.name} ?? this.${field.name}'),
-            );
-          }
+  for (final param in parameters) {
+    list.add(Method((m) => m
+      ..name = param.name
+      ..type = MethodType.getter
+      ..returns = refer('Color')));
+  }
 
-          b.addExpression(refer(className).call([], namedArgs).returned);
-        }),
-    ),
-    Method(
-      (m) => m
-        ..name = 'lerp'
-        ..annotations.add(refer('override'))
-        ..returns = refer(className)
-        ..requiredParameters.addAll([
-          Parameter(
-            (p) => p
-              ..name = 'other'
-              ..type = refer('ThemeExtension<$className>?'),
-          ),
-          Parameter(
-            (p) => p
-              ..name = 't'
-              ..type = refer('double'),
-          ),
-        ])
-        ..body = Block((b) {
-          b.statements.add(
-            Code('if (other is! $className) return this;'),
-          );
+  list.add(Method((m) => m
+    ..name = 'copyWith'
+    ..annotations.add(refer('override'))
+    ..returns = refer(className)
+    ..optionalParameters.addAll(parameters.map((p) => Parameter((p2) => p2
+      ..name = p.name!
+      ..type = refer('Color?')
+      ..named = true)))
+    ..body = refer('_$className').call([], {
+      for (final p in parameters) p.name!: refer(p.name!).ifNullThen(refer('this').property(p.name!))
+    }).code));
 
-          final namedArgs = <String, Expression>{};
-          for (final field in fields) {
-            namedArgs[field.name!] = CodeExpression(
-              Code('Color.lerp(${field.name}, other.${field.name}, t)!'),
-            );
-          }
+  list.add(Method((m) => m
+    ..name = 'lerp'
+    ..annotations.add(refer('override'))
+    ..returns = refer('ThemeExtension<$className>')
+    ..requiredParameters.addAll([
+      Parameter((p) => p..name = 'other'..type = refer('ThemeExtension<$className>?')),
+      Parameter((p) => p..name = 't'..type = refer('double')),
+    ])
+    ..body = Block.of([
+      Code('if (other is! $className) return this;'),
+      refer('_$className').call([], {
+        for (final p in parameters)
+          p.name!: refer('Color')
+              .property('lerp')
+              .call([refer(p.name!), refer('other').property(p.name!), refer('t')])
+              .nullChecked
+      }).returned.statement,
+    ])));
 
-          b.addExpression(
-            refer(className).call([], namedArgs).returned,
-          );
-        }),
-    ),
-    Method(
-      (b) => b
-        ..name = 'of'
-        ..static = true
-        ..returns = refer(className)
-        ..requiredParameters.add(
-          Parameter(
-            (p) => p
-              ..name = 'context'
-              ..type = refer('BuildContext'),
-          ),
-        )
-        ..body = refer('Theme')
-            .property('of')
-            .call([refer('context')])
-            .property('extension')
-            .call([], {}, [refer(className)])
-            .nullChecked
-            .code,
-    ),
-  ];
+  list.add(Method((m) => m
+    ..name = 'of'
+    ..static = true
+    ..returns = refer(className)
+    ..requiredParameters.add(Parameter((p) => p..name = 'context'..type = refer('BuildContext')))
+    ..body = refer('Theme')
+        .property('of')
+        .call([refer('context')])
+        .property('extension')
+        .call([])
+        .nullChecked
+        .returned
+        .statement));
+
+  return list;
 }
