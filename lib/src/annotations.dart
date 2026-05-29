73c4473a813330ae4ee33fdc94e5aa34b10243c7
Rerurate514 <rerurateyuto@gmail.com>
2026-05-29 18:22:57 +0900

    プロパティの削除

--- lib/src/annotations.dart
+++ lib/src/annotations.dart
@@ -1,11 +1,3 @@
-import 'package:flutter/material.dart';
-
 class ThemePalette {
-  const ThemePalette({
-    required this.brightness,
-    this.fromSeed
-  });
-  
-  final Brightness brightness;
-  final Color? fromSeed;
+  const ThemePalette();
 }
